f9458bc29091526cda1ed4148f781c2e6dab90bc
feng gu <fgu30@gatech.edu>
2020-11-14 22:01:40 -0500

    npm init

--- man.js
+++ man.js
@@ -1,27 +1,24 @@
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-type: 'line',
-data: {
-    labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-    datasets: [{
-    data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
-    lineTension: 0,
-    backgroundColor: 'transparent',
-    borderColor: '#007bff',
-    borderWidth: 4,
-    pointBackgroundColor: '#007bff'
-    }]
-},
-options: {
-    scales: {
-    yAxes: [{
-        ticks: {
-        beginAtZero: false
-        }
-    }]
-    },
-    legend: {
-    display: false,
-    }
-}
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  port:"3306",
+  user: "root",
+  password: "1234",
+  database:"sakila"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM actor", function (err, result) {
+    if (err) throw err;
+    var sql  = result;
+    console.log(sql.length);
+    for (var index = 0; index < sql.length; index++) {
+      console.log(sql[index].first_name)      
+  }
+  });
+
+  con.end();
+
 });
